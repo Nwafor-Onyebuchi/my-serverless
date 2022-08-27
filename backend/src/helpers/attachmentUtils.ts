d33d32462f69cdaffbebc956f54cd8019e4fe497
Nwafor-Onyebuchi <buchilazarus4@gmail.com>
2022-08-27 22:57:54 +0100

    add tracing

--- backend/src/helpers/attachmentUtils.ts
+++ backend/src/helpers/attachmentUtils.ts
@@ -1,6 +1,6 @@
-// import * as AWS from 'aws-sdk'
+import * as AWS from 'aws-sdk'
 // import * as AWSXRay from 'aws-xray-sdk'
+const AWSXRay = require('aws-xray-sdk')
 
-// const XAWS = AWSXRay.captureAWS(AWS)
+export const XAWS = AWSXRay.captureAWS(AWS)
 
-// TODO: Implement the fileStogare logic
